a33054044528c02972747f111898a7f0ad9da88f
leonard9 <kakuzops@gmail.com>
2019-01-24 09:10:50 -0200

    TESTE CARAI

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# vue-tutorial-todolist
+# vue-tutorial
+
+Vue Todo list
+
+
+### baseado no post da [Késsia Castro](https://medium.com/@kessiacastro/vue-js-tutorial-iniciando-com-componentes-4445b3eb0ffe)
+
+Um otimo texto e inicio simples para começar a entender de uma maneira sutil e facil o VUE JS.
---
+++ src/App.vue
@@ -0,0 +1,58 @@
+
+<template>
+  <div id="app">
+     <section class="hero is-dark is-bold is-fullheight">
+      <div class="hero-head">
+        <nav class="navbar">
+          <div class="container">
+            <div class="navbar-brand">
+              <a class="navbar-item">
+                VueJs todo-list
+              </a>
+            </div>
+          </div>
+        </nav>
+      </div>
+
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <div class="row">
+            <div class="columns">
+              <div class="column is-half is-offset-one-quarter">
+                <todo-card></todo-card>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="hero-foot">
+        <div class="container">
+          <div class="content has-text-centered">
+            <p>
+              <strong>Todo List</strong>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import TodoCard from './components/TodoCard'
+export default {
+  name: 'app',
+  components: {
+    TodoCard
+  },
+  data () {
+    return {
+
+}
+  }
+}
+</script>
+
+<style>
+</style>
